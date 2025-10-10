99ee9c1445e75205bfcd87c4d4979cb49e46bb94
Sorath <santiagoacostaavila2905@gmail.com>
2025-10-10 17:40:49 -0500

    Proyect21

--- frontend/config.js
+++ frontend/config.js
@@ -4,5 +4,5 @@ const BASE_URL = "https://granbar-bq.onrender.com";
 // ✅ Endpoints con prefijo /api (como en server.js)
 export const API_URL = `${BASE_URL}/api/productos`;
 export const API_BASE = `${BASE_URL}/api/ordenes`;
-export const API_REPORTES = `${BASE_URL}/api/reportes/enviar`;
+export const API_REPORTES = `${BASE_URL}/api/reportes`;
 export const API_AUTH_LOGIN = `${BASE_URL}/api/auth/login`;
